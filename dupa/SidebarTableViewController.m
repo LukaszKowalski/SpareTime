eee785354238a5b1c4d9f81678481a5c0fef1c10
Łukasz Kowalski <lukaszkowalski@ukaszs-Mac-mini.local>
2015-03-20 15:48:02 +0100

    sections

--- dupa/SidebarTableViewController.m
+++ dupa/SidebarTableViewController.m
@@ -11,6 +11,7 @@
 #import "firstCellTableViewCell.h"
 #import "separatorTableViewCell.h"
 #import "normalTableViewCell.h"
+#import "leftViewModel.h"
 
 #define Category_Box_Height 72
 #define Category_Subclass_Height 36
@@ -20,7 +21,7 @@
 @interface SidebarTableViewController ()
 
 @property (nonatomic, strong) NSMutableArray *expandedPaths;
-
+@property (nonatomic, strong) NSMutableSet *set_OpenIndex;
 
 @end
 
@@ -43,9 +44,11 @@
     serchTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Szukaj" attributes:@{NSForegroundColorAttributeName: color}];
     
     serchTextField.textColor = [UIColor whiteColor];
+    
     [self.search addSubview: serchTextField];
     
-    //    [self.view addSubview:self.tableView];
+    self.contentTableView.backgroundColor = [UIColor clearColor];
+    self.set_OpenIndex = [[NSMutableSet alloc] init];
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(resetTableView)
                                                  name:@"resetsideBar"
@@ -99,15 +102,15 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)sectionIndex
 {
-    
-    return 0;
+    if (sectionIndex == 0) {
+        return 100;
+    }
+    return 36;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (indexPath.row == 0) {
-        return 100;
-    }
-    if (indexPath.row % 2 != 0){
+
+    if (indexPath.row % 2 == 0){
         return 16;
     }
     if ([self.expandedPaths containsObject:indexPath]) {
@@ -129,9 +132,9 @@
         [self.expandedPaths addObject:indexPath];
     }
     
-    NSLog(@"komorki rozszerzone %@", [NSArray arrayWithObject:[NSIndexPath indexPathForRow:indexPath.row inSection:0]]);
+    NSLog(@"komorki rozszerzone %@", [NSArray arrayWithObject:[NSIndexPath indexPathForRow:indexPath.row inSection:indexPath.section]]);
     
-    [tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:indexPath.row inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
+    [tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:indexPath.row inSection:indexPath.section]] withRowAnimation:UITableViewRowAnimationNone];
     
     NSLog(@"DOTKNALEM %ld", (long)indexPath.row);
 
@@ -140,16 +143,99 @@
 #pragma mark -
 #pragma mark UITableView Datasource
 
-
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    if (section == 0) {
+        return 0;
+    }
+    if ([self.set_OpenIndex containsObject:[NSNumber numberWithInteger:section]]) {
+        return 5; // or what ever is the number of rows
+    }
+    
+    return 0;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 100)];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 5, tableView.frame.size.width, 18)];
+
+        label.text = [[leftViewModel sharedInstance] getSideBarCategory];
+        
+        UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+        UIVisualEffectView *visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blur];
+        
+        visualEffectView.frame = view.frame;
+        [view addSubview:visualEffectView];
+        [view insertSubview:label aboveSubview:view];
+        return view;
+    }
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 36)];
+    UIButton *headerClick = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 36)];
+    headerClick.tag = section;
+    headerClick.backgroundColor = [UIColor whiteColor];
+    /* Create custom view to display section header... */
+    [headerClick addTarget:self action:@selector(headerClicked:) forControlEvents:UIControlEventTouchUpInside];
+    headerClick.titleLabel.text = [NSString stringWithFormat:@"headerClick %ld", (long)section];
+   
+    [view addSubview:headerClick];
+    return view;
+}
+
+-(void)headerClicked:(UIButton *) sender{
+    NSLog(@"Kamil to kutas");
+
+//    NSNumber *sectionNumber = [NSNumber numberWithInteger:sender.tag];
+//    
+//    if(self.set_OpenIndex == nil) {
+//        self.set_OpenIndex = [[NSMutableSet alloc] init];
+//    }
+//
+//    if([self.set_OpenIndex containsObject:sectionNumber]) {
+//        [self.set_OpenIndex removeObject:sectionNumber];
+//
+//    } else {
+//        [self.set_OpenIndex addObject:sectionNumber];
+//    }
+
+    
+
+    if ([self.set_OpenIndex containsObject:[NSNumber numberWithUnsignedInteger:sender.tag]]) {
+        [self.set_OpenIndex removeObject:[NSNumber numberWithUnsignedInteger:sender.tag]];
+        [self.contentTableView reloadSections:[NSIndexSet indexSetWithIndex:sender.tag] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+    else{
+
+        if (self.set_OpenIndex.count > 0) {
+            //--- a header is opened already, close the previous one before opening the other
+
+            [UIView animateWithDuration:0.5 animations:^{
+                [self.set_OpenIndex enumerateObjectsUsingBlock:^(id obj, BOOL *stop){
+                    [self.set_OpenIndex removeObject:obj];
+                    [self.contentTableView reloadSections:[NSIndexSet indexSetWithIndex:[obj integerValue]] withRowAnimation:UITableViewRowAnimationAutomatic];
+
+                }];
+            } completion:^(BOOL finished){
+
+                [self.set_OpenIndex addObject:[NSNumber numberWithUnsignedInteger:sender.tag]];
+                [self.contentTableView reloadSections:[NSIndexSet indexSetWithIndex:sender.tag] withRowAnimation:UITableViewRowAnimationAutomatic];
+
+            }];
+        }
+        else{
+            [self.set_OpenIndex addObject:[NSNumber numberWithUnsignedInteger:sender.tag]];
+            [self.contentTableView reloadSections:[NSIndexSet indexSetWithIndex:sender.tag] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+    }
+
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -171,17 +257,17 @@
     separatorTableViewCell *separatorCell = [tableView dequeueReusableCellWithIdentifier:separatorCellIdentifier];
     normalTableViewCell *normalCell = [tableView dequeueReusableCellWithIdentifier:normalCellIdentifier];
     
-    if (indexPath.row == 0) {
-        [tableView registerNib:[UINib nibWithNibName:@"firstCell" bundle:nil] forCellReuseIdentifier:firstCellIdentifier];
-        cell = [tableView dequeueReusableCellWithIdentifier:firstCellIdentifier];
-        return cell;
-    }
-    if (indexPath.row % 2 != 0) {
+//    if (indexPath.row == 1) {
+//        [tableView registerNib:[UINib nibWithNibName:@"firstCell" bundle:nil] forCellReuseIdentifier:firstCellIdentifier];
+//        cell = [tableView dequeueReusableCellWithIdentifier:firstCellIdentifier];
+//        return cell;
+//    }
+    if (indexPath.row % 2 == 0) {
         [tableView registerNib:[UINib nibWithNibName:@"separator" bundle:nil] forCellReuseIdentifier:separatorCellIdentifier];
         separatorCell = [tableView dequeueReusableCellWithIdentifier:separatorCellIdentifier];
         return separatorCell;
     }
-    if (indexPath.row != 0 && indexPath.row % 2 == 0) {
+    if (indexPath.row % 2 != 0) {
         [tableView registerNib:[UINib nibWithNibName:@"normalCell" bundle:nil] forCellReuseIdentifier:normalCellIdentifier];
         normalCell = [tableView dequeueReusableCellWithIdentifier:normalCellIdentifier];
         return normalCell;
